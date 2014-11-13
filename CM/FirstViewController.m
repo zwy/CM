e6a36dad733b34223fff58a29cb32bd0f1e33c1d
zwy <zwy0615@qq.com>
2014-11-13 14:00:01 +0800

    通讯录设置修改

--- CM/FirstViewController.m
+++ CM/FirstViewController.m
@@ -20,11 +20,15 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.title = @"管理";
     
-    [self createContact];
+    
     
     [self createTableViewDataSouce];
 }
-
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self createContact];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
